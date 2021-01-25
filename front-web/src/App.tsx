657d9f47815c8008a6667e52760d93eb43f62074
joaoneivaneto <joaoneivanetos@gmail.com>
2021-01-25 14:32:27 -0300

    add routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css';
-import Navbar from './Navbar';
-import Home from './Home';
+
+import Routes from './Routes';
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-    </>
+   <Routes />    
+    
   );
 }
 
